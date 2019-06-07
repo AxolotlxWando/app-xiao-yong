99d56844ba3873d0f070262f957c6b6fa5716838
Wesley Wang <zakudesune@gmail.com>
2019-06-07 15:24:13 +1000

    Fix bug in reading NEO4j with lower case \'j\'s instead of all capitals from the config

--- config/dbNeo4j.js
+++ config/dbNeo4j.js
@@ -1,8 +1,8 @@
 const dbNeo4j = {
   connection: {
-    host: process.env.DB_NEO4j_HOST || 'bolt://localhost:17687',
-    user: process.env.DB_NEO4j_USER || 'neo4j',
-    password: process.env.DB_NEO4j_PASSWORD || ''
+    host: process.env.DB_NEO4J_HOST || 'bolt://localhost:17687',
+    user: process.env.DB_NEO4J_USER || 'neo4j',
+    password: process.env.DB_NEO4J_PASSWORD || ''
   }
 };
 
